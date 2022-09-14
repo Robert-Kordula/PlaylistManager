aee25efa2972fccbb69fee8566a97b8f5655985a
Robert-Kordula <bobbykordula@gmail.com>
2022-09-14 21:19:35 +0100

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const App = () => {
+    return <h1>Here is your list of playlists!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
