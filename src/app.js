ee90c86e284397f80ac48b3c9cb12558452e50e3
Robert <bobbykordula@gmail.com>
2022-09-28 13:52:51 +0100

    Setup env variables

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ debug: true, path: '../.env'});
 
 import express from 'express';
 import request from 'request';
@@ -7,8 +7,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import axios from 'axios';
 
+const client_id = process.env.REACT_APP_CLIENT_ID;
+const client_secret = process.env.REACT_APP_CLIENT_SECRET;
+
 console.log(process.env);
-
-const client_id = process.env.CLIENT_ID;
-const client_secret = process.env.CLIENT_SECRET;
-
