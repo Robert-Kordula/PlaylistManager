e490c65d953f7f74f6ea718b5c44e9029b28bcd3
Robert-Kordula <bobbykordula@gmail.com>
2022-10-18 23:13:31 +0100

    Testing

---
+++ website/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            <Route>
+                
+            </Route>
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById('app-root'),
+);
